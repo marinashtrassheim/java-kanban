da59bdc8157d16a14f2c34002559b1219b80a94e
marinashtrassheim <73815942+marinashtrassheim@users.noreply.github.com>
2025-03-22 13:58:17 +0500

    Add files via upload

---
+++ src/task/Epic.java
@@ -0,0 +1,51 @@
+package task;
+import java.util.ArrayList;
+/**
+ * Класс Epic представляет собой задачу типа "Эпик", которая может содержать подзадачи (SubTask).
+ * Эпик имеет статус, который зависит от статусов его подзадач.
+ */
+ public class Epic extends Task{
+    // Список подзадач, относящихся к этому эпику
+    private ArrayList<SubTask> subtasks = new ArrayList<SubTask>();
+
+    /**
+     * Конструктор для создания эпика, когда список подзадач неизвестен.
+     *
+     * @param name        Название эпика
+     * @param description Описание эпика
+     */
+    public Epic(String name, String description) {
+        super(name, description);
+        this.status = Status.NEW; // По умолчанию статус NEW
+    }
+    /**
+     * Метод для получения списка подзадач, относящихся к этому эпику.
+     * @return Список подзадач
+     */
+    public ArrayList<SubTask> getSubtasks() {
+        return subtasks;
+    }
+    /**
+     * Метод для установки списка подзадач, относящихся к этому эпику.
+     */
+    public void setSubtasks(SubTask subtask) {
+        subtasks.add(subtask);
+    }
+
+    /**
+     * Переопределение метода toString для удобного вывода информации об эпике.
+     *
+     * @return Строковое представление эпика в формате:
+     * "Эпик {ID=..., Название=..., Описание=..., Статус=..., Подзадачи=...}"
+     */
+    @Override
+    public String toString() {
+        return "Эпик {" +
+                "ID=" + id +
+                ", Название='" + name + '\'' +
+                ", Описание='" + description + '\'' +
+                ", Статус=" + status +
+                ", Подзадачи=" + subtasks +
+                '}';
+    }
+}
